60f196469a26d608aa94259a87992aab5631944a
srikesh15 <srikeshgopidi@gmail.com>
2026-05-25 11:44:37 +0530

    stats_screen

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -1,9 +1,28 @@
 import { View, Text } from 'react-native';
+import {useState} from 'react';
+import useTransactionStore from '../store/useTransactionStore';
+
 
 export default function StatsScreen() {
+  const [selectedDate,setSelectedDate] = useState(new Date());
+
+  const transactions = useTransactionStore((state) => state.transactions);
+
+  const year = selectedDate.getFullYear();
+  const month = selectedDate.getMonth();
+  const monthName = selectedDate.toLocaleString('default', { month: 'long' });
+
+  const monthlyTransactions = transactions.filter((tx) => {
+    const d = new Date(tx.date);
+    return d.getFullYear() === year && d.getMonth() === month;
+  });
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>StatsScreen</Text>
+      <Text>
+        {monthName} {year}
+      </Text>
     </View>
   );
 }
+    
